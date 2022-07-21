82c53547267db6524455134e397e6dce36dbaccd
Ine Arts <29948516+IneMEGAmaxi@users.noreply.github.com>
2022-07-21 17:24:17 +0200

    clean up MologramBlockRenderer to make it easier to change the way the beam is rendered

--- src/main/java/be/uantwerpen/scicraft/renderer/MologramBlockRenderer.java
+++ src/main/java/be/uantwerpen/scicraft/renderer/MologramBlockRenderer.java
@@ -7,6 +7,9 @@ import be.uantwerpen.scicraft.particle.Particles;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
@@ -16,8 +19,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3f;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.*;
 import net.minecraft.world.World;
 import org.apache.commons.lang3.ObjectUtils;
 
@@ -32,22 +35,31 @@ public class MologramBlockRenderer implements BlockEntityRenderer<MologramBlockE
 
     @Override
     public void render(MologramBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-        Random random = new Random();
         World world = entity.getWorld();
-
         if (world == null) return;
 
-        BlockPos pos = entity.getPos();
         stack = entity.getInventory().getStack(0);
-
         if (stack.isEmpty()) return;
 
-        matrices.push();
+        BlockPos pos = entity.getPos();
+        // render beam
+        renderBeam(world, pos);
+        // Render stack inside the block
+        renderInsideOfBlock(matrices, vertexConsumers, light, overlay);
+    }
+    private static void renderBeam(World world, BlockPos pos) {
+        renderBeamAsParticle(world, pos);
+        //renderBeam()
+    }
+    private static void renderBeamAsParticle(World world, BlockPos pos){
+        Random random = new Random();
         if(random.nextDouble()<0.2f){
             world.addParticle(Particles.HOLOGRAM_PARTICLE, pos.getX()+ 0.5d, pos.getY() + 1.75d, pos.getZ() + 0.5d,
                     0,0,0);
         }
-        // Move the item
+    }
+    private static void renderInsideOfBlock(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay){
+        matrices.push();
         if (Block.getBlockFromItem(stack.getItem()) != Blocks.AIR) matrices.translate(0.5, 0, 0.5);
         else {
             matrices.translate(0.5, 0.10, 0.64);
@@ -58,4 +70,59 @@ public class MologramBlockRenderer implements BlockEntityRenderer<MologramBlockE
         // Mandatory call after GL calls
         matrices.pop();
     }
+    // code for beacon beam rendering --> inspiration
+    /*
+    public static void renderBeam(MatrixStack matrices, VertexConsumerProvider vertexConsumers, Identifier textureId, float tickDelta, float heightScale, long worldTime, int yOffset, int maxY, float[] color, float innerRadius, float outerRadius) {
+        int i = yOffset + maxY;
+        matrices.push();
+        matrices.translate(0.5, 0.0, 0.5);
+        float f = (float)Math.floorMod(worldTime, 40) + tickDelta;
+        float g = maxY < 0 ? f : -f;
+        float h = MathHelper.fractionalPart(g * 0.2F - (float)MathHelper.floor(g * 0.1F));
+        float j = color[0];
+        float k = color[1];
+        float l = color[2];
+        matrices.push();
+        matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(f * 2.25F - 45.0F));
+        float m = 0.0F;
+        float p = 0.0F;
+        float q = -innerRadius;
+        float r = 0.0F;
+        float s = 0.0F;
+        float t = -innerRadius;
+        float u = 0.0F;
+        float v = 1.0F;
+        float w = -1.0F + h;
+        float x = (float)maxY * heightScale * (0.5F / innerRadius) + w;
+        renderBeamLayer(matrices, vertexConsumers.getBuffer(RenderLayer.getBeaconBeam(textureId, false)), j, k, l, 1.0F, yOffset, i, 0.0F, innerRadius, innerRadius, 0.0F, q, 0.0F, 0.0F, t, 0.0F, 1.0F, x, w);
+        matrices.pop();
+        m = -outerRadius;
+        float n = -outerRadius;
+        p = -outerRadius;
+        q = -outerRadius;
+        u = 0.0F;
+        v = 1.0F;
+        w = -1.0F + h;
+        x = (float)maxY * heightScale + w;
+        renderBeamLayer(matrices, vertexConsumers.getBuffer(RenderLayer.getBeaconBeam(textureId, true)), j, k, l, 0.125F, yOffset, i, m, n, outerRadius, p, q, outerRadius, outerRadius, outerRadius, 0.0F, 1.0F, x, w);
+        matrices.pop();
+    }
+    private static void renderBeamLayer(MatrixStack matrices, VertexConsumer vertices, float red, float green, float blue, float alpha, int yOffset, int height, float x1, float z1, float x2, float z2, float x3, float z3, float x4, float z4, float u1, float u2, float v1, float v2) {
+        MatrixStack.Entry entry = matrices.peek();
+        Matrix4f matrix4f = entry.getPositionMatrix();
+        Matrix3f matrix3f = entry.getNormalMatrix();
+        renderBeamFace(matrix4f, matrix3f, vertices, red, green, blue, alpha, yOffset, height, x1, z1, x2, z2, u1, u2, v1, v2);
+        renderBeamFace(matrix4f, matrix3f, vertices, red, green, blue, alpha, yOffset, height, x4, z4, x3, z3, u1, u2, v1, v2);
+        renderBeamFace(matrix4f, matrix3f, vertices, red, green, blue, alpha, yOffset, height, x2, z2, x4, z4, u1, u2, v1, v2);
+        renderBeamFace(matrix4f, matrix3f, vertices, red, green, blue, alpha, yOffset, height, x3, z3, x1, z1, u1, u2, v1, v2);
+    }
+    private static void renderBeamFace(Matrix4f positionMatrix, Matrix3f normalMatrix, VertexConsumer vertices, float red, float green, float blue, float alpha, int yOffset, int height, float x1, float z1, float x2, float z2, float u1, float u2, float v1, float v2) {
+        renderBeamVertex(positionMatrix, normalMatrix, vertices, red, green, blue, alpha, height, x1, z1, u2, v1);
+        renderBeamVertex(positionMatrix, normalMatrix, vertices, red, green, blue, alpha, yOffset, x1, z1, u2, v2);
+        renderBeamVertex(positionMatrix, normalMatrix, vertices, red, green, blue, alpha, yOffset, x2, z2, u1, v2);
+        renderBeamVertex(positionMatrix, normalMatrix, vertices, red, green, blue, alpha, height, x2, z2, u1, v1);
+    }
+    private static void renderBeamVertex(Matrix4f positionMatrix, Matrix3f normalMatrix, VertexConsumer vertices, float red, float green, float blue, float alpha, int y, float x, float z, float u, float v) {
+        vertices.vertex(positionMatrix, x, (float)y, z).color(red, green, blue, alpha).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(15728880).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
+    }*/
 }
